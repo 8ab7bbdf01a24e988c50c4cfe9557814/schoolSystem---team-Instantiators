603b2a7185bc85e5e0162ac2527bb2dafd1d290a
maxy8ken <156901573+maxy8ken@users.noreply.github.com>
2025-06-19 15:30:52 +1000

    Add files via upload

--- staff.py
+++ staff.py
@@ -1,3 +1,15 @@
 #Create a Staff class in this file
 #Accept and assign agreed parameters in the constructor and define agreed public methods in the class
 #Program the class so that users of the class only need to interact with its methods
+from person import *
+class Staff(Person):
+    
+    def __init__(self, name, salary):
+        super().__init__(name)
+        self.salary = salary
+
+    def giveRaise(self, salary, amount):
+        self.salary = salary + amount
+
+    def info(self, salary):
+        return (f"Hey Students My name is {self.name} and I make ")
