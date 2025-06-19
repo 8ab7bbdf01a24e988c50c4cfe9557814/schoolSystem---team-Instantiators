4ac9b965152c32328e3d1b9f1890d2de89d7ba9e
Lachysagit <lachlan_carlton@outlook.com>
2025-06-19 15:23:28 +1000

    Update student.py

--- student.py
+++ student.py
@@ -1,3 +1,16 @@
-#Create a Student class in this file
+
+class Student(Person):
+    def __init__(self, name, schoolYear):
+        super().__init__(name)
+        self.schoolYear = schoolYear
+      
+    
+    def getSchoolYear(self):
+        return self.schoolYear 
+    def setSchoolYear(self, schoolYear):
+        self.schoolYear = schoolYear
+    def nextSchoolYear(self):
+        self.schoolYear = self.schoolYear+1
+        #Create a Student class in this file
 #Accept and assign agreed parameters in the constructor and define agreed public methods in the class
 #Program the class so that users of the class only need to interact with its methods
