39935859c48c3216bf4efc41b6541a564c54a93e
ikeboneslachy <ike.afanasiev@education.nsw.gov.au>
2025-06-19 15:38:29 +1000

    Add files via upload

---
+++ PrimaryStudent.py
@@ -0,0 +1,20 @@
+class primaryStudent():
+    def __init__(self, name, schoolYear, teacher):
+        self.name = name
+        self.schoolYear = schoolYear
+        self.teacher = teacher
+
+    def setTeacher(self, teacher):
+        self.teacher = teacher
+    def getTeacher(self, teacher):
+        self.teacher = teacher
+    def info(self, name, schoolYear, teacher):
+        return (f"my name is {self.name}")
+
+        
+
+
+
+
+
+
