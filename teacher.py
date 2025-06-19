1200936326388cee71b67ec6500ee5e616acffae
123soaps <leon.soper@education.nsw.gov.au>
2025-06-19 15:07:21 +1000

    Update teacher.py

--- teacher.py
+++ teacher.py
@@ -1,3 +1,9 @@
 #Create a Teacher class in this file
 #Accept and assign agreed parameters in the constructor and define agreed public methods in the class
 #Program the class so that users of the class only need to interact with its methods
+
+class Teacher:
+  def __init__(self, salary, subjects):
+    self.subjects = []
+  def enroll(subjects):
+    
