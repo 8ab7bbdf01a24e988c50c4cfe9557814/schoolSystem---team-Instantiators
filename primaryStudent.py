0ffff3d96e04f02150d5d632dd1e5585865898ff
maxy8ken <156901573+maxy8ken@users.noreply.github.com>
2025-06-19 15:40:20 +1000

    Update primaryStudent.py

--- primaryStudent.py
+++ primaryStudent.py
@@ -1,3 +1,16 @@
 #Create a PrimaryStudent class in this file
 #Accept and assign agreed parameters in the constructor and define agreed public methods in the class
 #Program the class so that users of the class only need to interact with its methods
+
+class primaryStudent():
+    def __init__(self, name, schoolYear, teacher):
+        self.name = name
+        self.schoolYear = schoolYear
+        self.teacher = teacher
+
+    def setTeacher(self, teacher):
+        self.teacher = teacher
+    def getTeacher(self, teacher):
+        self.teacher = teacher
+    def info(self, name, schoolYear, teacher):
+        return (f"my name is {self.name}")
