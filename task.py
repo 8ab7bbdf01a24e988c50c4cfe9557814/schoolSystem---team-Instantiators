80417563a7882cd4d63749da7df75d920f5a75ee
123soaps <leon.soper@education.nsw.gov.au>
2025-06-24 09:33:47 +1000

    Update task.py

--- task.py
+++ task.py
@@ -1,3 +1,21 @@
 #Create a Task class in this file
 #Accept and assign agreed parameters in the constructor and define agreed public methods in the class
 #Program the class so that users of the class only need to interact with its methods
+
+class Task:
+  def __init__(self, name, maxMark, marks):
+    self.name = name
+    self.maxMark = maxMark
+    self.marks = {}
+  def setmark(self, student, mark):
+    self.marks[student] = mark 
+  def getmark(self, mark):
+    return self.mark
+  def info(self, name):
+    print(f"This task is " + self.name)
+  def Max(self, marks):
+    highest = max(marks)
+  def Avg(self, marks):
+    median = sum(marks) / len(marks)
+  def Min(self, marks):
+    lowest = min(marks)
